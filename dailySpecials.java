42b24f7a1a9952d6207ec4095b82a98cd0a48807
Jinlong0212 <jinlongluo0212@yahoo.com>
2025-06-05 15:34:59 -0700

    Add files via upload

--- dailySpecials.java
+++ dailySpecials.java
@@ -17,141 +17,152 @@ public class dailySpecials {
 //        input.close();
         // in the world of coffee, I need a name of a coffee beverage and a price.
 
-        boolean saturday = specials.equals("Saturday");
-        boolean sunday = specials .equals("Sunday");
+        boolean saturday = specials.equalsIgnoreCase("Saturday");
+        boolean sunday = specials.equalsIgnoreCase("Sunday");
 
-        if(saturday || sunday) {
-            System.out.println("Please enter a weekday, not weekend!");
-            specials = input.next();
-        }
 
         String coffee;
         double price, totalPrice;
 
-        switch (specials) {
+        while(saturday || sunday) {
+            System.out.println("Please enter a weekday, not weekend!");
+            specials = input.next();
 
-            // my cases will be the day of the week
-            case "Monday" -> {
-                coffee = "Latte";
-                price = 4.95;
-                System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+            saturday = specials.equalsIgnoreCase("Saturday");
+            sunday = specials.equalsIgnoreCase("Sunday");
 
-                System.out.println("How many "+coffee+"s would you like to oder?");
-                amounts = input.nextInt();
-                totalPrice =amounts * price;
-
-                if(amounts == 0) {
-                    System.out.println("Looks like you don't like "+coffee+"s! So sad!!!");
-
-                } else if (amounts == 1) {
-                    System.out.println("Looks like you will be ordering only "+amounts+" "+coffee+" today!");
-
-                }else {
-
-                    System.out.print(amounts+" "+coffee+"s have been ordered totalling $");
-                    System.out.printf("%.2f", totalPrice);
-                    System.out.println(" dollars!");
-
-                }
-            }// end of case.
-            case "Tuesday" -> {
-                coffee = "Frappe";
-                price = 5.95;
-                System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
-
-                System.out.println("How many "+coffee+"s would you like to oder?");
-                amounts = input.nextInt();
-                totalPrice =amounts * price;
-
-                if(amounts == 0) {
-                    System.out.println("Looks like you don't like "+coffee+"s! So sad!!!");
-
-                } else if (amounts == 1) {
-                    System.out.println("Looks like you will be ordering only "+amounts+" "+coffee+" today!");
-
-                }else {
-
-                    System.out.print(amounts+" "+coffee+"s have been ordered totalling $");
-                    System.out.printf("%.2f", totalPrice);
-                    System.out.println(" dollars!");
-
-                }
-
-            }// end of case.
-            case "Wednesday" -> {
-                coffee = "Cappuccino";
-                price = 4.35;
-                System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
-
-                System.out.println("How many "+coffee+"s would you like to oder?");
-                amounts = input.nextInt();
-                totalPrice =amounts * price;
-
-                if(amounts == 0) {
-                    System.out.println("Looks like you don't like "+coffee+"s! So sad!!!");
-
-                } else if (amounts == 1) {
-                    System.out.println("Looks like you will be ordering only "+amounts+" "+coffee+" today!");
-
-                }else {
-
-                    System.out.print(amounts+" "+coffee+"s have been ordered totalling $");
-                    System.out.printf("%.2f", totalPrice);
-                    System.out.println(" dollars!");
-
-                }
-
-            }// end of case.
-            case "Thursday" -> {
-                coffee = "Regular Joe";
-                price = 2.95;
-                System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
-
-                System.out.println("How many "+coffee+"s would you like to oder?");
-                amounts = input.nextInt();
-                totalPrice =amounts * price;
-
-                if(amounts == 0) {
-                    System.out.println("Looks like you don't like "+coffee+"s! So sad!!!");
-
-                } else if (amounts == 1) {
-                    System.out.println("Looks like you will be ordering only "+amounts+" "+coffee+" today!");
-
-                }else {
-
-                    System.out.print(amounts+" "+coffee+"s have been ordered totalling $");
-                    System.out.printf("%.2f", totalPrice);
-                    System.out.println(" dollars!");
-
-                }
-
-            }// end of case.
-            case "Friday" -> {
-                coffee = "Green Tea Latte";
-                price = 6.95;
-                System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
-
-                System.out.println("How many "+coffee+"s would you like to oder?");
-                amounts = input.nextInt();
-                totalPrice =amounts * price;
-
-                if(amounts == 0) {
-                    System.out.println("Looks like you don't like "+coffee+"s! So sad!!!");
-
-                } else if (amounts == 1) {
-                    System.out.println("Looks like you will be ordering only "+amounts+" "+coffee+" today!");
-
-                }else {
-
-                    System.out.print(amounts+" "+coffee+"s have been ordered totalling $");
-                    System.out.printf("%.2f", totalPrice);
-                    System.out.println(" dollars!");
-
-                }
-
-            }// end of case.
-            default -> System.out.println("Please enter a valid day, or check your spelling!");
         }
+
+
+            switch (specials.toLowerCase()) {
+
+                // my cases will be the day of the week
+                case "monday" -> {
+                    coffee = "Latte";
+                    price = 4.95;
+                    System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+
+                    System.out.println("How many " + coffee + "s would you like to oder?");
+                    amounts = input.nextInt();
+                    totalPrice = amounts * price;
+
+                    if (amounts == 0) {
+                        System.out.println("Looks like you don't like " + coffee + "s! So sad!!!");
+
+                    } else if (amounts == 1) {
+                        System.out.println("Looks like you will be ordering only " + amounts + " " + coffee + " today!");
+
+                    } else {
+
+                        System.out.print(amounts + " " + coffee + "s have been ordered totalling $");
+                        System.out.printf("%.2f", totalPrice);
+                        System.out.println(" dollars!");
+
+                    }
+                }// end of case.
+                case "tuesday" -> {
+                    coffee = "Frappe";
+                    price = 5.95;
+                    System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+
+                    System.out.println("How many " + coffee + "s would you like to oder?");
+                    amounts = input.nextInt();
+                    totalPrice = amounts * price;
+
+                    if (amounts == 0) {
+                        System.out.println("Looks like you don't like " + coffee + "s! So sad!!!");
+
+                    } else if (amounts == 1) {
+                        System.out.println("Looks like you will be ordering only " + amounts + " " + coffee + " today!");
+
+                    } else {
+
+                        System.out.print(amounts + " " + coffee + "s have been ordered totalling $");
+                        System.out.printf("%.2f", totalPrice);
+                        System.out.println(" dollars!");
+
+                    }
+
+                }// end of case.
+                case "wednesday" -> {
+                    coffee = "Cappuccino";
+                    price = 4.35;
+                    System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+
+                    System.out.println("How many " + coffee + "s would you like to oder?");
+                    amounts = input.nextInt();
+                    totalPrice = amounts * price;
+
+                    if (amounts == 0) {
+                        System.out.println("Looks like you don't like " + coffee + "s! So sad!!!");
+
+                    } else if (amounts == 1) {
+                        System.out.println("Looks like you will be ordering only " + amounts + " " + coffee + " today!");
+
+                    } else {
+
+                        System.out.print(amounts + " " + coffee + "s have been ordered totalling $");
+                        System.out.printf("%.2f", totalPrice);
+                        System.out.println(" dollars!");
+
+                    }
+
+                }// end of case.
+                case "thursday" -> {
+                    coffee = "Regular Joe";
+                    price = 2.95;
+                    System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+
+                    System.out.println("How many " + coffee + "s would you like to oder?");
+                    amounts = input.nextInt();
+                    totalPrice = amounts * price;
+
+                    if (amounts == 0) {
+                        System.out.println("Looks like you don't like " + coffee + "s! So sad!!!");
+
+                    } else if (amounts == 1) {
+                        System.out.println("Looks like you will be ordering only " + amounts + " " + coffee + " today!");
+
+                    } else {
+
+                        System.out.print(amounts + " " + coffee + "s have been ordered totalling $");
+                        System.out.printf("%.2f", totalPrice);
+                        System.out.println(" dollars!");
+
+                    }
+
+                }// end of case.
+                case "friday" -> {
+                    coffee = "Green Tea Latte";
+                    price = 6.95;
+                    System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+
+                    System.out.println("How many " + coffee + "s would you like to oder?");
+                    amounts = input.nextInt();
+                    totalPrice = amounts * price;
+
+                    if (amounts == 0) {
+                        System.out.println("Looks like you don't like " + coffee + "s! So sad!!!");
+
+                    } else if (amounts == 1) {
+                        System.out.println("Looks like you will be ordering only " + amounts + " " + coffee + " today!");
+
+                    } else {
+
+                        System.out.print(amounts + " " + coffee + "s have been ordered totalling $");
+                        System.out.printf("%.2f", totalPrice);
+                        System.out.println(" dollars!");
+
+                    }
+
+                }// end of case.
+
+                default -> System.out.println("Please enter a valid day, or check your spelling!");
+            }
+
+
+
+
 
 
     }
